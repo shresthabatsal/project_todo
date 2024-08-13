1ad497f900e0bc9298f6baccbbb90b6e09c70c5d
batsal <shresthabatsal247@gmail.com>
2024-08-13 20:15:10 +0545

    Update task displaying logic

--- src/model/DashboardModel.java
+++ src/model/DashboardModel.java
@@ -3,14 +3,14 @@ package model;
 public class DashboardModel {
 
     public String getTodayQuery() {
-        return "SELECT * FROM tasks WHERE user_id = ? AND due_date = CURDATE()";
+        return "SELECT * FROM tasks WHERE user_id = ? AND due_date = CURDATE() AND is_complete = 0";
     }
 
     public String getImportantQuery() {
-        return "SELECT * FROM tasks WHERE user_id = ? AND is_important = true";
+        return "SELECT * FROM tasks WHERE user_id = ? AND is_important = true AND is_complete = 0";
     }
 
     public String getUpcomingQuery() {
-        return "SELECT * FROM tasks WHERE user_id = ? AND due_date > CURDATE()";
+        return "SELECT * FROM tasks WHERE user_id = ? AND due_date > CURDATE() AND is_complete = 0";
     }
 }
