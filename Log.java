25adee337ea0d8fe7251fc63495d5e12e465b310
OskarGiri <149762116+OskarGiri@users.noreply.github.com>
2024-08-17 20:33:37 +0545

    Update login button function

--- Log.java
+++ Log.java
@@ -30,6 +30,21 @@ public class Log {
         frame.add(loginButton);
         frame.add(successLabel);
 
+        // Action listener for the login button
+        loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String username = userText.getText();
+                String password = new String(passwordText.getPassword());
+                
+                // Simple hardcoded validation (just for example)
+                if(username.equals("user") && password.equals("password123")) {
+                    successLabel.setText("Login successful!");
+                } else {
+                    successLabel.setText("Login failed.");
+                }
+            }
+        });
+
         // Set the frame visibility to true
         frame.setVisible(true);
     }
