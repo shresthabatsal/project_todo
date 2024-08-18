7334b6becc0425d1c9100eed3bee819eb6a4b4c9
OskarGiri <149762116+OskarGiri@users.noreply.github.com>
2024-08-18 13:31:24 +0545

    Delete misc directory

--- misc/TaskToDo.sql
+++
@@ -1,9 +0,0 @@
-create database TaskToDo;
-use TaskToDo;
-CREATE TABLE tasks (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255),
-    description TEXT,
-    due_date DATE,
-    repeat_interval VARCHAR(50)
-);
